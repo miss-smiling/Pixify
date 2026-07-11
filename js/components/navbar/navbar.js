22816acc45f7d64a66bbecb5b249d132a7815c0f
Neha.R <neharane841@gmail.com>
2026-07-11 20:25:51 +0530

    Add Navbar component with HTML structure

---
+++ js/components/navbar/navbar.js
@@ -0,0 +1,37 @@
+export function Navbar() {
+
+    return `
+
+<header class="navbar">
+
+    <div class="container">
+
+        <div class="navbar-wrapper">
+
+            <div class="navbar-logo">
+
+                Logo
+
+            </div>
+
+            <nav class="navbar-menu">
+
+                Navigation
+
+            </nav>
+
+            <div class="navbar-actions">
+
+                Actions
+
+            </div>
+
+        </div>
+
+    </div>
+
+</header>
+
+`;
+
+}
