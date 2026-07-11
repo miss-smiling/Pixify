81d7c5f329e28f65e03777b1bbf996bf9ffc0e57
Neha.R <neharane841@gmail.com>
2026-07-11 20:14:06 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * ==========================================
+ * Pixify
+ * Application Entry Point
+ * Version: 1.0
+ * ==========================================
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🌸 Pixify initialized successfully.");
+});
