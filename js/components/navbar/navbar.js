f08a3a78c7c748617bba419faa5824cf5339d663
Neha.R <neharane841@gmail.com>
2026-07-12 11:58:49 +0530

    Update navbar.js

--- js/components/navbar/navbar.js
+++ js/components/navbar/navbar.js
@@ -5,34 +5,71 @@ export function Navbar() {
         <div class="container navbar-container">
 
             <a href="#" class="logo">
-                🌸 Pixify
+
+                <img
+                    src="assets/branding/logo.svg"
+                    alt="Pixify Logo"
+                    class="logo-icon"
+                >
+
+                <span class="logo-text">
+                    Pixify
+                </span>
+
             </a>
 
             <nav class="nav-links">
 
-                <a href="#">Home</a>
-                <a href="#">Shop</a>
-                <a href="#">Collections</a>
-                <a href="#">About</a>
-                <a href="#">Donate</a>
+                <a href="#" class="nav-link active">Home</a>
+
+                <a href="#" class="nav-link">Shop</a>
+
+                <a href="#" class="nav-link">Collections</a>
+
+                <a href="#" class="nav-link">About</a>
+
+                <a href="#" class="nav-link">Donate</a>
 
             </nav>
 
             <div class="nav-actions">
 
-                <input
-                    type="search"
-                    class="search-box"
-                    placeholder="Search themes..."
-                >
+                <div class="search-wrapper">
+
+                    <img
+                        src="assets/icons/search.svg"
+                        class="search-icon"
+                        alt=""
+                    >
+
+                    <input
+                        type="search"
+                        placeholder="Search themes..."
+                        class="search-input"
+                    >
+
+                </div>
 
                 <button class="cart-btn">
-                    🛒
-                    <span class="cart-count">0</span>
+
+                    <img
+                        src="assets/icons/cart.svg"
+                        alt=""
+                    >
+
+                    <span class="cart-badge">0</span>
+
                 </button>
 
-                <button class="login-btn">
-                    Sign In
+                <button class="signin-btn">
+
+                    <img
+                        src="assets/icons/user.svg"
+                        alt=""
+                    >
+
+                    <span>Sign In</span>
+
                 </button>
 
             </div>
